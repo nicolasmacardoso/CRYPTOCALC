389aafdadabcde0dfa8d7e4b6d5ad4020f3510fa
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2025-01-13 19:16:31 -0300

    Adição dos arquivos

---
+++ script.js
@@ -0,0 +1,35 @@
+function calculaCripto () {
+    $("#resultado").text('')
+
+    const salinicial   = parseFloat($("#salinicial").val());
+    const criptcompra  = parseFloat($("#criptcompra").val());
+    const criptmax     = parseFloat($("#criptmax").val());
+    const intervalo    = parseFloat($("#intervalo").val());
+    const dolar        = parseFloat($("#dolar").val());
+    
+    const criptprecos = Array.from({ length: Math.floor((criptmax - criptcompra) / intervalo) + 1 }, (_, i) => criptcompra + i * intervalo);
+    
+    const lucros = [];
+    
+    criptprecos.forEach(criptnovopreco => {
+        const novosaldo = salinicial * (criptnovopreco / criptcompra);
+        const lucro = novosaldo - salinicial;
+        const percentlucro = Math.round((lucro / salinicial) * 100);
+    
+        lucros.push({
+            percentual: percentlucro,
+            lucroliq: `R$ ${ (lucro * dolar).toFixed(2) }`,
+            valtt: `R$ ${ (novosaldo * dolar).toFixed(2) }`,
+            precocript:`$${ criptnovopreco.toFixed(2) }, R$ ${ (criptnovopreco * dolar).toFixed(2) })`
+        });
+    });
+    
+    lucros.forEach(lucro => {
+        $("#resultado").append(`
+            <h3>Lucro de ${lucro.percentual}%</h3>
+            <span>Lucro líquido: ${lucro.lucroliq}</span>
+            <span>Valor total: ${lucro.valtt}</span>
+            <span>Preço da criptomoeda: ${lucro.precocript}</span>
+        `)        
+    })
+}
