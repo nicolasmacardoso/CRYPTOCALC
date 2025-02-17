9ea5a7d4bf9e1f8b35a64194f894117ae924d016
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2025-02-17 12:05:27 -0300

    Pegando cotação do dólar automaticamente e adicionando opção de quantidade de tokens

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 function calculaCripto () {
-    $("#resultado").text('')
+    fetch("https://api.exchangerate-api.com/v4/latest/USD")
+        .then((response) => response.json())
+        .then((data) => {
+            $("#resultado").text('')
 
-    const salinicial   = parseFloat($("#salinicial").val());
-    const criptcompra  = parseFloat($("#criptcompra").val());
-    const criptmax     = parseFloat($("#criptmax").val());
-    const intervalo    = parseFloat($("#intervalo").val());
-    const dolar        = parseFloat($("#dolar").val());
-    
-    const criptprecos = Array.from({ length: Math.floor((criptmax - criptcompra) / intervalo) + 2 }, (_, i) => criptcompra + i * intervalo);
-    
-    const lucros = [];
-    
-    criptprecos.forEach(criptnovopreco => {
-        const novosaldo = salinicial * (criptnovopreco / criptcompra);
-        const lucro = novosaldo - salinicial;
-        const percentlucro = Math.round((lucro / salinicial) * 100);
-    
-        lucros.push({
-            percentual: percentlucro,
-            lucroliq: `R$ ${ (lucro * dolar).toFixed(2) }, $ ${ (lucro).toFixed(2) }`,
-            valtt: `R$ ${ (novosaldo * dolar).toFixed(2) }, $ ${ (novosaldo).toFixed(2) }`,
-            precocript:` R$ ${ (criptnovopreco * dolar).toFixed(2) }, $${ criptnovopreco.toFixed(2) })`
+            const tiposalini   = parseInt($("#tiposalini").val());
+            const salinicial   = tiposalini == 1 ? parseFloat($("#salinicial").val()) : parseFloat($("#salinicial").val()) * parseFloat($("#criptcompra").val());
+            const criptcompra  = parseFloat($("#criptcompra").val());
+            const criptmax     = parseFloat($("#criptmax").val());
+            const intervalo    = parseFloat($("#intervalo").val());
+            const dolar        = data.rates.BRL 
+
+            const criptprecos = Array.from({ length: Math.floor((criptmax - criptcompra) / intervalo) + 1 }, (_, i) => criptcompra + i * intervalo);
+            
+            const lucros = [];
+            
+            criptprecos.forEach(criptnovopreco => {
+                const novosaldo = salinicial * (criptnovopreco / criptcompra);
+                const lucro = novosaldo - salinicial;
+                const percentlucro = Math.round((lucro / salinicial) * 100);
+            
+                lucros.push({
+                    percentual: percentlucro,
+                    lucroliq: `R$ ${ (lucro * dolar).toFixed(2) } ----- $ ${ lucro.toFixed(2) }`,
+                    valtt: `R$ ${ (novosaldo * dolar).toFixed(2) } ----- $ ${ (novosaldo).toFixed(2) }`,
+                    precocript:`R$ ${ (criptnovopreco * dolar).toFixed(2) } ----- $ ${ criptnovopreco.toFixed(2) }`
+                });
+            });
+            
+            lucros.forEach(lucro => {
+                $("#resultado").append(`
+                    <div class="info-result">
+                        <h3>Lucro de ${lucro.percentual}%</h3>
+                        <span>Lucro lÃ­quido: ${lucro.lucroliq}</span>
+                        <span>Valor total: ${lucro.valtt}</span>
+                        <span>PreÃ§o da criptomoeda: ${lucro.precocript}</span>
+                    </div>
+                `)        
+            })
         });
-    });
-    
-    lucros.forEach(lucro => {
-        $("#resultado").append(`
-            <div class="info-container">
-                <h4>Lucro de ${lucro.percentual}%</h4>
-
-                <span>Lucro líquido: ${lucro.lucroliq}</span>
-                <span>Valor total: ${lucro.valtt}</span>
-                <span>Preço da criptomoeda: ${lucro.precocript}</span>
-            </div>
-        `)        
-    })
 }
